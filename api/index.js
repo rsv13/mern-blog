2f31065e4991100c37e0be774eb6aba4738dd79b
rsv13 <rsv130393@gmail.com>
2024-07-24 03:37:22 +0100

    create signup api routes

--- api/index.js
+++ api/index.js
@@ -1,11 +1,13 @@
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
+import authRoutes from "./routes/auth.route.js";
 import userRoutes from "./routes/user.route.js";
 
 dotenv.config();
 
 const app = express();
+app.use(express.json());
 
 mongoose
   .connect(process.env.MONGO)
@@ -21,3 +23,4 @@ app.listen(3000, () => {
 });
 
 app.use("/api/user", userRoutes);
+app.use("/api/auth", authRoutes);
