67e20b0baae9ad3024d05de92682ef2fff2e89b6
rsv13 <rsv130393@gmail.com>
2024-07-25 19:39:59 +0100

    Add update User API Route

--- api/index.js
+++ api/index.js
@@ -1,3 +1,4 @@
+import cookieParser from "cookie-parser";
 import dotenv from "dotenv";
 import express from "express";
 import mongoose from "mongoose";
@@ -8,6 +9,7 @@ dotenv.config();
 
 const app = express();
 app.use(express.json());
+app.use(cookieParser());
 
 mongoose
   .connect(process.env.MONGO)
