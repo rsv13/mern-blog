7f7b0200092d2e03c4e0747fe6ffde9ce8eaecd4
rsv13 <rsv130393@gmail.com>
2024-07-27 23:50:47 +0100

    Create get posts API route

--- api/routes/post.route.js
+++ api/routes/post.route.js
@@ -1,9 +1,10 @@
 import express from "express";
-import { createPost } from "../controllers/post.controller.js";
+import { createPost, getPost } from "../controllers/post.controller.js";
 import { verifyToken } from "../utils/verifyUser.js";
 
 const router = express.Router();
 
 router.post("/createPost", verifyToken, createPost);
+router.get("/getPost", getPost);
 
 export default router;
